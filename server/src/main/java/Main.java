0e5e770567cd28e2ae95f48c4d38ff9d1e43ff63
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-10-07 20:27:29 -0600

    Wohoo! getting started done
    we have the basic server running, which means now we have to do stuff for which there is no guide. kinda scary, kinda cool

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,29 @@
 import chess.*;
+import spark.*;
+import java.util.*;
+import server.Server;
+
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        Server server = new Server();
+        server.run(8080);
+
+
     }
+//    private static void createServer() {
+//        Spark.port(8080);
+//        Spark.staticFiles.location("/public");
+//        createRoutes();
+//        Spark.awaitInitialization();
+//        System.out.println("listening on port " + 8000);
+//    }
+//
+//
+//    private static void createRoutes() {
+//        Spark.get("/hello", ((request, response) -> "I really need to go to the bathroom"));
+//    }
 }
+
---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,29 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        //Spark.init();
+        createRoutes();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+    private static void createRoutes() {
+        Spark.get("/hello", ((request, response) -> "I really need to go to the bathroom"));
+   }
+}
