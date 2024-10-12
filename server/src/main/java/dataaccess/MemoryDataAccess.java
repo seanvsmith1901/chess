493a1075de92d63c359d7e161f74421f5a9cc337
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-10-11 18:52:53 -0600

    thing I've implemented everything I need in order to clear the database, kind of
    working on the return statements for the later half

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
+import com.google.gson.Gson;
+import model.*;
+import service.AuthService;
+import service.GameService;
+import service.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class MemoryDataAccess {
+
+    Map<String, AuthData> authenticationTokens = new HashMap<String, AuthData>();
+    Map<String, GameData> gameTokens = new HashMap<String, GameData>();
+    Map<String, UserData> userTokens = new HashMap<String, UserData>();
+
+    public void deleteEverything() {
+        authenticationTokens.clear();
+        gameTokens.clear();
+        userTokens.clear();
+    }
+
+    // so this is where I actually have to like, store everything which is going to be annoying.
+    // I need to be able to implement the three different kinds of model data structures as like a hash map or something
+    // and then we can have a function that just uhh throws back nothing to show that we have nothing in there.
+
 }
