ff656ac194300ff2b9a93803a6457978df53c23d
seanvsmith1901 <seanvsmith1901@gmail.com>
2024-11-25 22:09:23 -0700

    updated to string
    of the position method haha

--- shared/src/main/java/websocket/commands/Position.java
+++ shared/src/main/java/websocket/commands/Position.java
@@ -11,4 +11,27 @@ public class Position {
 
     public int getRow() { return row; }
     public int getCol() { return col; }
+
+    @Override
+    public String toString() {
+        return intToStrRow(col)  + row;
+    }
+
+    private String intToStrRow(int rowNum) {
+        return switch (rowNum) {
+            case 1 -> "A";
+            case 2 -> "B";
+            case 3 -> "C";
+            case 4 -> "D";
+            case 5 -> "E";
+            case 6 -> "F";
+            case 7 -> "G";
+            case 8 -> "H";
+            default ->
+                // Handle invalid input (if the number is not between 1 and 8)
+                    "Invalid input";
+        };
+    }
+
+
 }
